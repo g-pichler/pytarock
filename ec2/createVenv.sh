2a4d1299b4605c5fef9ba8199564f8b870512775
Georg Pichler <georg.pichler@gmail.com>
2021-12-11 15:27:49 +0100

    fixed install command for venv

--- ec2/createVenv.sh
+++ ec2/createVenv.sh
@@ -4,6 +4,6 @@ rm -rf /opt/venv
 python3 -m venv /opt/venv || exit 1
 
 source /opt/venv/bin/activate || exit 1
-python3 -m pip -r /opt/pytarock/requirements.txt || exit 1
+python3 -m pip install -r /opt/pytarock/requirements.txt || exit 1
 
 exit 0
